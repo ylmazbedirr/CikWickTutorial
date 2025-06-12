d63a1ccf6e9e4fb49a2bdc88ab079ddb83c963cf
bedir <96594753+ylmazbedirr@users.noreply.github.com>
2025-06-12 18:42:58 +0300

    v0.4 Camera Follow System Added

--- Assets/_GameAssets/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/_GameAssets/Scripts/Gameplay/Player/PlayerController.cs
@@ -10,6 +10,8 @@ public class PlayerController : MonoBehaviour
     [Header("Movement Settings")]
     [SerializeField] private float _movementSpeed;
 
+    [SerializeField] private KeyCode _movementKey;
+
 
     [Header("Jump Settings")]
     [SerializeField] private KeyCode _jumpKey;
@@ -17,15 +19,26 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private bool _canJump;
     [SerializeField] private float _jumpCoolDown;
 
+
+    [Header("Sliding Settings")]
+    [SerializeField] private KeyCode _slideKey;
+    // slide hızı
+    [SerializeField] private float _slideMultiplier;
+    [SerializeField] private float _slideDrag;
+
     [Header("Ground Check Settings")]
 
     [SerializeField] private float _playerHeight;
     [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private float _groundDrag;
     private Rigidbody _playerRigidBody;
 
     private float _horizontalInput, _verticalInput;
 
     private Vector3 _movementDirection;
+
+    private bool _isSliding;
+
 
     private void Awake()
     {
@@ -49,6 +62,18 @@ public class PlayerController : MonoBehaviour
         _horizontalInput = Input.GetAxisRaw("Horizontal");
         _verticalInput = Input.GetAxisRaw("Vertical");
 
+        if (Input.GetKeyDown(_slideKey))
+        {
+            _isSliding = true;
+            Debug.Log("Player Sliding");
+        }
+        else if (Input.GetKeyDown(_movementKey))
+        {
+            _isSliding = false;
+            Debug.Log("Player Moving!");
+        }
+
+
         if (Input.GetKey(_jumpKey) && _canJump & IsGrounded())
         {
             _canJump = false;
@@ -62,7 +87,45 @@ public class PlayerController : MonoBehaviour
     {
         _movementDirection = _orientationTransform.forward * _verticalInput + _orientationTransform.right * _horizontalInput;
 
-        _playerRigidBody.AddForce(_movementDirection.normalized * _movementSpeed, ForceMode.Force);
+        if (_isSliding)
+        {
+            _playerRigidBody.AddForce(_movementDirection.normalized * _movementSpeed * _slideMultiplier, ForceMode.Force);
+
+        }
+        else
+        {
+            _playerRigidBody.AddForce(_movementDirection.normalized * _movementSpeed, ForceMode.Force);
+
+        }
+
+    }
+
+
+    private void SetPlayerDrag()
+    {
+        if (_isSliding)
+        {
+            _playerRigidBody.linearDamping = _slideDrag;
+        }
+        else
+        {
+            _playerRigidBody.linearDamping = _groundDrag;
+        }
+
+
+    }
+
+    // anti hile sistemi
+    private void LimitPlayerSpeed()
+    {
+        Vector3 flatVelocity = new Vector3(_playerRigidBody.linearVelocity.x, 0f, _playerRigidBody.linearVelocity.z);
+
+        if (flatVelocity.magnitude > _movementSpeed)
+        {
+            Vector3 limitedVelocity = flatVelocity.normalized * _movementSpeed;
+            _playerRigidBody.linearVelocity
+                = new Vector3(limitedVelocity.x, _playerRigidBody.linearVelocity.y, limitedVelocity.z);
+        }
     }
 
 
